203cb7136f6a0570bed8ec57496b380d44b00811
kapiziak <nznajomypl@gmail.com>
2022-10-08 22:26:56 +0200

    Added cors headers

--- src/main/java/dziala/mnie/dla/hackathon/HackathonApplication.java
+++ src/main/java/dziala/mnie/dla/hackathon/HackathonApplication.java
@@ -2,6 +2,9 @@ package dziala.mnie.dla.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HackathonApplication {
@@ -11,6 +14,16 @@ public class HackathonApplication {
         final String ANSI_GREEN = "\u001B[32m";
         final String ANSI_RESET = "\u001B[0m";
         System.out.print("\nServer boot " + ANSI_GREEN + "COMPLETED" + ANSI_RESET + ".\n");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
     }
 
 }
